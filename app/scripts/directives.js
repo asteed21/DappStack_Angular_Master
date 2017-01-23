c1220d1fb1e785a1e8bdb68276a9492cca1e57cf
asteed21 <asteed21@gmail.com>
2017-01-23 18:06:34 +0800

    Updates to view templates, build thumbnail

---
+++ app/scripts/directives.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('dappstackAngularMasterApp')
+
+.directive('dappThumbnail', function() {
+    return {
+        restrict: 'EA',
+        templateUrl: 'thumbnail.html',
+    }
+})
+;
